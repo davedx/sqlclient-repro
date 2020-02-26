755f9ac2a43c422b275015d28ba9633956e3d741
Dave Clayton <dave.clayton@leaseplan.com>
2020-02-26 08:21:23 +0100

    Repro

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+                Console.WriteLine("Blog: "+blog.Url);
+            }
+        }
+    }
+}
